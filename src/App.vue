4243d708359aa5081fa82d71c172cce2d2dd137f
Andrey Shurygin <andrey.sh@solargroup.team>
2025-10-13 21:58:49 +0300

    Переход на TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { API_ROUTES } from './api';
+import ProfileAvatar from './components/ProfileAvatar.vue';
+import type { Profile } from './interfaces/profile';
+
+const profile = ref<Profile>();
+
+const fetchProfile = async () => {
+  const data = await fetch(API_ROUTES.profile);
+  const res = (await data.json()) as Profile;
+  profile.value = res;
+};
+
+onMounted(() => {
+  fetchProfile();
+});
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="flex gap-4 min-h-[calc(100vh-140px)] max-w-[1450px] mt-[140px] mx-auto">
+    <nav class="min-w-[400px]">
+      <ProfileAvatar v-if="profile" :name="profile.name" />
+    </nav>
+    <main>Контент</main>
+  </div>
 </template>
-
-<style scoped></style>
---
+++ src/components/ProfileAvatar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { name } = defineProps<{ name: string }>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-6">
+    <img class="self-start" src="/avatar.png" alt="Avatar" />
+    <div class="text-[18px] tracking-[0.36px]">
+      Привет, <strong>{{ name }}</strong>
+    </div>
+  </div>
+</template>
